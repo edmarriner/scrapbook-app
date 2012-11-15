4bc27970a0d1a220cd465f56110e0f18b25f302b
edmarriner <edmarriner@gmail.com>
2012-11-15 23:12:36 +0000

    tidyed up methods to open and close menu

--- script.js
+++ script.js
@@ -5,34 +5,37 @@ window.onload = function(){
 	var menu		=	doc.getElementById('menu');
     var page		=	doc.getElementById('page');
     var pageBlock	=	doc.getElementById('page-block');
+    var docWidth;
 
     // Set click event to open the menu
 	doc.getElementById('header-menu-button').addEventListener('click', toggleMenu, false);
     
     // Set click event to open the menu
-	pageBlock.addEventListener('click', pageBlockClick, false);
+	pageBlock.addEventListener('click', closeMenu, false);
 
 	// Function to open the menu
 	function toggleMenu() {
 		if(hasClass(menu,'open')){
-			removeClass(menu,'open');
-			pageBlock.style.display = 'none';
-	        page.style.webkitTransform = 'translate3d(0px, 0 , 0)';
-	    	menu.className += " closed";
-
+			closeMenu();
 		}else{
-			removeClass(menu,'closed');
-	        page.style.webkitTransform = 'translate3d(200px, 0 , 0)';
-	    	menu.className += " open";
-	    	pageBlock.style.display = 'block';
+			openMenu();
 		}
     }
 
-    function pageBlockClick() {
-		removeClass(menu,'open');
+    // Close the menu
+    function closeMenu() {
+    	removeClass(menu,'open');
 		pageBlock.style.display = 'none';
         page.style.webkitTransform = 'translate3d(0px, 0 , 0)';
     	menu.className += " closed";
+    }
+
+     // Open the menu
+    function openMenu() {
+    	removeClass(menu,'closed');
+		pageBlock.style.display = 'block';
+        page.style.webkitTransform = 'translate3d(200px, 0 , 0)';
+    	menu.className += " open";
     }
 
     // hasClass helper method
