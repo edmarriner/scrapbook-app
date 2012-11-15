7803835f37e9f0955c1c18a54fe7fd67f70420eb
edmarriner <edmarriner@gmail.com>
2012-11-15 22:27:46 +0000

    Will now close the menu wWhen you click anywhere on the main content or header

--- script.js
+++ script.js
@@ -1,24 +1,38 @@
 window.onload = function(){
 
 	// cache DOM elements
-	var doc		=	document;
-	var menu	=	doc.getElementById('menu');
-    var page 	=	doc.getElementById('page');
+	var doc			=	document;
+	var menu		=	doc.getElementById('menu');
+    var page		=	doc.getElementById('page');
+    var pageBlock	=	doc.getElementById('page-block');
 
     // Set click event to open the menu
 	doc.getElementById('header-menu-button').addEventListener('click', toggleMenu, false);
+    
+    // Set click event to open the menu
+	pageBlock.addEventListener('click', pageBlockClick, false);
 
 	// Function to open the menu
 	function toggleMenu() {
 		if(hasClass(menu,'open')){
 			removeClass(menu,'open');
+			pageBlock.style.display = 'none';
 	        page.style.webkitTransform = 'translate3d(0px, 0 , 0)';
 	    	menu.className += " closed";
+
 		}else{
 			removeClass(menu,'closed');
 	        page.style.webkitTransform = 'translate3d(200px, 0 , 0)';
 	    	menu.className += " open";
+	    	pageBlock.style.display = 'block';
 		}
+    }
+
+    function pageBlockClick() {
+		removeClass(menu,'open');
+		pageBlock.style.display = 'none';
+        page.style.webkitTransform = 'translate3d(0px, 0 , 0)';
+    	menu.className += " closed";
     }
 
     // hasClass helper method
